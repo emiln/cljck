70625188dcb1f4002fe5ee91d203b2e963d2f0b7
Emil Holm Nauerby <emil@nauerby.com>
2015-08-29 15:07:21 +0200

    [#7] Move mouse cursor to position.
    This commit adds the ability to move the mouse cursor to a given position
    and changes the interaction with the event queue slightly. Adding an event
    is done by putting a vector like [:click] or [:move-to 100 200] on the
    publicly available channel in the io namespace. Whether this is how it's
    going to stay is an open question, but it works well for now.

--- src/cljck/io.clj
+++ src/cljck/io.clj
@@ -1,7 +1,8 @@
 (ns cljck.io
-  (:require [clojure.core.async :as async])
+  (:require [clojure.core.async :refer [<! chan go-loop]])
   (:import  [java.awt Robot]
             [java.awt.event InputEvent]))
+
 (def button-map
   "A mapping from Clojure keywords to more long-winded Java enums."
   {:left   InputEvent/BUTTON1_DOWN_MASK
@@ -12,7 +13,6 @@
   "You need a robot to do stuff with the mouse. This is that robot."
   (Robot.))
 
-
 (defn click [k]
   "Simulates a mouse click of the key denoted by the keyword k in the
   button-map. A click is really a press and a release in succession."
@@ -20,3 +20,31 @@
     (doto robot
       (.mousePress i)
       (.mouseRelease i))))
+
+(defn move-to [x y]
+  "Moves the mouse cursor to the absolute position [x y] on the screen."
+  (.mouseMove robot x y))
+
+(def event-channel (chan))
+
+(defmulti process-event
+  "Processes an event pushed onto the event channel. As events can take on
+  open-ended shapes as Cljck is extended, this is a multi method free to be
+  supplied with additional implementations.
+
+  The argument is expected to be a vector like [:move-to 100 100] or [:click].
+  Dispatches on first."
+  first)
+
+(defmethod process-event :click
+  [_]
+  (click :left))
+
+(defmethod process-event :move-to
+  [[_ x y]]
+  (move-to x y))
+
+;; Process events as they arrive on the event channel.
+(go-loop []
+  (process-event (<! event-channel))
+  (recur))
